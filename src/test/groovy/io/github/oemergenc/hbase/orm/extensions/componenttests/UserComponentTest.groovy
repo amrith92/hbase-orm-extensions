95e5ed91cb5ca2f0d1d7cb157eda00b7a609811c
Oemer Genc <oemergenc@gmail.com>
2020-03-06 21:34:51 +0100

    fix empty or null dynamic column values break serialization

--- src/test/groovy/io/github/oemergenc/hbase/orm/extensions/componenttests/UserComponentTest.groovy
+++ src/test/groovy/io/github/oemergenc/hbase/orm/extensions/componenttests/UserComponentTest.groovy
@@ -3,9 +3,10 @@ package io.github.oemergenc.hbase.orm.extensions.componenttests
 import io.github.oemergenc.hbase.orm.extensions.dao.InvalidUserDao
 import io.github.oemergenc.hbase.orm.extensions.dao.ValidUserDao
 import io.github.oemergenc.hbase.orm.extensions.exception.DuplicateColumnIdentifierException
+import spock.lang.Unroll
 
-import static io.github.oemergenc.hbase.orm.extensions.data.UserContent.address
-import static io.github.oemergenc.hbase.orm.extensions.data.UserContent.invalidrecord
+import static io.github.oemergenc.hbase.orm.extensions.data.UserContent.*
+import static java.util.UUID.randomUUID
 
 class UserComponentTest extends AbstractComponentSpec {
     def validUserDao = new ValidUserDao(bigTableHelper.connect())
@@ -26,5 +27,36 @@ class UserComponentTest extends AbstractComponentSpec {
 
         then:
         thrown(DuplicateColumnIdentifierException)
+    }
+
+    @Unroll
+    def "empty or null dynamic columns value do not break persistence"() {
+        given:
+        def userId = randomUUID() as String
+        def validUserRecord = validrecord(userId: userId,
+                workAddresses: workAdress,
+                homeAddresses: homeAdress,
+        )
+
+        when:
+        validUserDao.persist(validUserRecord)
+
+        and:
+        def record = validUserDao.get(userId)
+
+        then:
+        noExceptionThrown()
+        record
+        record.workAddresses.collect { it.workAddress } == expectedWork
+        record.homeAddresses.collect { it.homeAddress } == expectedHome
+
+        where:
+        workAdress                            | homeAdress                                | expectedWork    | expectedHome
+        [workAddress(address: "workAddress")] | [homeAddress(address: "my-home-address")] | ["workAddress"] | ["my-home-address"]
+        [workAddress(address: "workAddress")] | []                                        | ["workAddress"] | []
+        []                                    | [homeAddress(address: "my-home-address")] | []              | ["my-home-address"]
+        null                                  | [homeAddress(address: "my-home-address")] | []              | ["my-home-address"]
+        [workAddress(address: "workAddress")] | null                                      | ["workAddress"] | []
+        [workAddress(address: "workAddress")] | []                                        | ["workAddress"] | []
     }
 }
